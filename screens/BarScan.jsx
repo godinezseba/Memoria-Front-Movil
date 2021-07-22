31d9e2db0f743f2272e84f958589cf47cf543b10
Sebastian Godinez <sebastian.godinez@hotmail.com>
2021-07-21 22:23:42 -0400

    feat(product-view): add buttons

--- screens/BarScan.jsx
+++ screens/BarScan.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import Constants from 'expo-constants';
 import { useLazyQuery, gql } from '@apollo/client';
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { BarCodeScanner, Constants } from 'expo-barcode-scanner';
 
 const GET_PRODUCT = gql`
 query GetProduct($id: String!){
@@ -16,27 +16,41 @@ query GetProduct($id: String!){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 30,
   },
   text: {
     color: 'red',
-  }
+  },
+  boxSectin: {
+    alignSelf: 'center',
+    backgroundColor: 'red',
+  },
+  cameraButton: {
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'transparent'
+  },
+  iconButton: {
+    color: "#fff",
+    fontSize: 40,
+  },
 });
 
-export const  BarScan = ({ navigation }) => {
+export const BarScan = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [barCode, setBarCode] = useState('');
+  const [cameraType, setCameraType] = useState(Constants.Type.back);
   const [scanned, setScanned] = useState(false);
   const [getProduct] = useLazyQuery(GET_PRODUCT, {
-    onCompleted: ({product}) => {
+    onCompleted: ({ product }) => {
       setScanned(false);
       navigation.push('Product', { product });
     },
     onError: ({ message }) => {
       setScanned(false);
-      alert(`Error al buscar el código!\nDetalle: ${message}\n${Constants.manifest.extra.API_URL}`);
+      alert(`Error al buscar el código!\nDetalle: ${message}`);
     }
   });
 
@@ -54,28 +68,65 @@ export const  BarScan = ({ navigation }) => {
   const handleSearchBarCode = () => {
     if (barCode !== '') {
       setScanned(true);
-      getProduct({variables: { id: barCode }});
+      getProduct({ variables: { id: barCode } });
     } else {
       alert(`Error, no se ingreso un código!`);
     }
   };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+  const handleCameraType = () => {
+    setCameraType(Constants.Type.back
+      ? Constants.Type.front
+      : Constants.Type.back);
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+
+  if (hasPermission === null) {
+    return <Text> Requesting for camera permission </Text>;
+  }
+  else if (hasPermission === false) {
+    return <Text> No access to camera </Text>;
   }
 
   return (
-  <BarCodeScanner
-    onBarCodeScanned={handleBarCodeScanned}
-    style={StyleSheet.absoluteFillObject}
-  >
-    <View style={styles.container}>
-      <Text style={styles.text}>{barCode || 'sin resultados...'}</Text>
-      <Button title={'Haga click para buscar'} onPress={handleSearchBarCode} />
+    <View style={{ flex: 1 }}>
+      <BarCodeScanner
+        onBarCodeScanned={handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+        type={cameraType}
+      >
+        <View style={styles.container}>
+          <View
+            style={styles.cameraButton}
+          >
+            <Ionicons
+              name="copy-outline"
+              style={{ ...styles.iconButton, opacity: 0 }}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.cameraButton}
+            onPress={handleSearchBarCode}
+          >
+            <FontAwesome
+              name="camera"
+              style={styles.iconButton}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cameraButton}
+            onPress={handleCameraType}
+          >
+            <MaterialCommunityIcons
+              name="camera-switch"
+              style={styles.iconButton}
+            />
+          </TouchableOpacity>
+        </View>
+        {/* <View style={styles.container}>
+          <Text style={styles.text}>{barCode || 'sin resultados...'}</Text>
+          <Button title={'Haga click para buscar'} onPress={handleSearchBarCode} />
+        </View> */}
+      </BarCodeScanner>
     </View>
-  </BarCodeScanner>
   );
 }
