c50319dab7e1902f66357d82d150eee9b4ea820c
Sebastian Godinez <sebastian.godinez@hotmail.com>
2021-07-28 21:17:26 -0400

    feat(product-view): add skeleton of product view

--- src/screens/ProductInfo.jsx
+++ src/screens/ProductInfo.jsx
@@ -1,27 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Accordion, Center, Box, Icon, Tabs } from 'native-base';
 
-const defaultProduct = { barcode_id: '',  brand: '', category: '', model: '', rating_data: { efficiency: '' }};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+const defaultProduct = {
+  barcode_id: '',
+  name: 'example',
+  brand: '',
+  category: '',
+  model: '',
+  rating_data: {
+    efficiency: '',
   },
-});
+};
 
 export const Product = ({ route }) => {
-  const product = route.params.product ? route.params.product : defaultProduct;
+  const [typeResult, setTypeResult] = useState(0);
+  const product = route.params?.product ? route.params.product : defaultProduct;
 
   return (
-    <View style={styles.container}>
-      <Text>{product.name}</Text>
-      {/* <Text>Eficiencia: {product.rating_data.efficiency}</Text>
-      <Text>Modelo: {product.model}</Text>
-      <Text>Marca: {product.brand}</Text>
-      <Text>Categoria: {product.category}</Text> */}
-    </View>
+    <Center flex={1}>
+      <Tabs onChange={setTypeResult} index={typeResult} align="center">
+        <Tabs.Bar>
+          <Tabs.Tab>Global</Tabs.Tab>
+          <Tabs.Tab>Categorico</Tabs.Tab>
+        </Tabs.Bar>
+      </Tabs>
+      <Box>
+        {product.name}
+      </Box>
+      <Box m={3}>
+        <Accordion index={[0, 1]}>
+          <Accordion.Item>
+            <Accordion.Summary>
+              ¿Cómo se obtuvo este resultado?
+              <Icon name="snowflake" type="MaterialCommunityIcons" />
+            </Accordion.Summary>
+            <Accordion.Details>
+              {typeResult === 0 ? 'Valores globales' : 'Valores en su categoria'}
+            </Accordion.Details>
+          </Accordion.Item>
+          <Accordion.Item>
+            <Accordion.Summary>
+              Sobre la empresa...
+              <Accordion.Icon />
+            </Accordion.Summary>
+            <Accordion.Details>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </Accordion.Details>
+          </Accordion.Item>
+        </Accordion>
+      </Box>
+    </Center>
   );
 };
