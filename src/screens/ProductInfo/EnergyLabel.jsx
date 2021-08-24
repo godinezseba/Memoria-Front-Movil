482b156c7a2007692d9bd4ebf45c14b51d290737
Sebastian Godinez <sebastian.godinez@hotmail.com>
2021-08-24 01:07:11 -0400

    feat(product-view): fade effect

--- src/screens/ProductInfo/EnergyLabel.jsx
+++ src/screens/ProductInfo/EnergyLabel.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Row, Column, Box } from 'native-base';
+import React, { useRef, useEffect } from 'react';
+import { Row, Column, Box, Fade } from 'native-base';
 
 import { Label, BlackLabel } from '../../components/Labels';
 import { numberToChar } from '../../constants/labels';
@@ -10,12 +10,12 @@ export const EnergyLabel = (props) => {
   let label = props.label || '1';
   label = numberToChar[label];
 
-  return (
+  const Labels = () => (
     <Box my={5}>
       <Column alignItems="center" space={3}>
         <Box w="100%">
           {letters.map((character) => (
-            <Row key={`label-${label}-${character}`} space={5} justifyContent="space-between">
+            <Row key={`label-${label}-${character}`} justifyContent="space-between">
               <Label character={character} />
               {label === character ? (
                 <BlackLabel character={character} />
@@ -27,5 +27,25 @@ export const EnergyLabel = (props) => {
         </Box>
       </Column>
     </Box>
-  )
+  );
+
+  // fade effect in the first render
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+  });
+
+  if (firstUpdate)
+    return (
+      <Fade in>
+        <Labels />
+      </Fade>
+    );
+
+  return (
+    <Labels />
+  );
 }
